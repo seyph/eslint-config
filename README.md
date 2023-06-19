7e18e6ec910c69586545345b51906685d3e86d62
Henrique Serraglia <ricks@duck.com>
2023-06-19 12:33:08 -0300

    Init @seyph/eslint-config

---
+++ README.md
@@ -0,0 +1,29 @@
+# SEYPH ESLint config
+
+## Whats included?
+
+- Airbnb config base;
+- Typescript parser and plugin;
+- React and Hooks plugins;
+- Import plugins;
+- JSX a11y plugin;
+- Tailwind CSS plugin;
+- Prettier integration;
+
+## Setup
+
+1. Install the dependencies
+
+```
+npm i -D eslint @seyph/eslint-config
+```
+
+2. Create a `.eslintrc.json` file extending the config:
+
+```
+{
+  "extends": "@seyph"
+}
+```
+
+> You can also use `.eslintrc.js` or `.eslintrc` instead of JSON if you prefer.
---
+++ index.js
@@ -0,0 +1,52 @@
+const base = require('./lib/base.js');
+const react = require('./lib/react.js');
+
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+    'plugin:tailwindcss/recommended',
+  ],
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    'simple-import-sort',
+    'import',
+    'react',
+    'jsx-a11y',
+    '@typescript-eslint',
+    'tailwindcss',
+  ],
+  rules: {
+    ...base,
+    ...react,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      alias: {
+        map: [['src', './src']],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+      },
+    },
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    },
+  },
+};
