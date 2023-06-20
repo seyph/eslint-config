0339dc138f86e07ccecdc5cd0a5131c43d8c8bc2
Henrique Serraglia <ricks@duck.com>
2023-06-20 09:15:02 -0300

    Added prettier configs

--- index.js
+++ index.js
@@ -2,54 +2,56 @@ const base = require('./lib/base.js');
 const react = require('./lib/react.js');
 
 module.exports = {
-  extends: [
-    'airbnb',
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'plugin:prettier/recommended',
-    'plugin:tailwindcss/recommended',
-  ],
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-    node: true,
-    jest: true,
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 'latest',
-    project: ['**/tsconfig.json'],
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  plugins: [
-    'simple-import-sort',
-    'import',
-    'react',
-    'jsx-a11y',
-    '@typescript-eslint',
-    'tailwindcss',
-  ],
-  rules: {
-    ...base,
-    ...react,
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-    'import/resolver': {
-      alias: {
-        map: [['src', './src']],
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
-      },
-    },
-    'import/parsers': {
-      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
-    },
-  },
-  ignorePatterns: ['dist/', '.next/', 'node_modules', 'scripts', 'examples'],
+	extends: [
+		'airbnb',
+		'airbnb-typescript',
+		'airbnb/hooks',
+		'plugin:prettier/recommended',
+		'plugin:tailwindcss/recommended',
+	],
+	env: {
+		browser: true,
+		commonjs: true,
+		es2021: true,
+		node: true,
+		jest: true,
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 'latest',
+		project: ['**/tsconfig.json'],
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	plugins: [
+		'simple-import-sort',
+		'import',
+		'react',
+		'jsx-a11y',
+		'prettier',
+		'@typescript-eslint',
+		'tailwindcss',
+	],
+	rules: {
+		...base,
+		...react,
+		'prettier/prettier': 'error',
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+		'import/resolver': {
+			alias: {
+				map: [['src', './src']],
+				extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+			},
+		},
+		'import/parsers': {
+			[require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+		},
+	},
+	ignorePatterns: ['dist/', '.next/', 'node_modules', 'scripts', 'examples'],
 };
